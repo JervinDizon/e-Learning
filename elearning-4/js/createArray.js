d2cb539906106e540a0290e21087a6ae687788b4
jervindizon <jervin.dizon@gmail.com>
2015-10-14 05:40:00 +0800

    Revision One

---
+++ elearning-4/js/createArray.js
@@ -0,0 +1,27 @@
+for (var i = 1; i <= totalChapters; i++) {
+	if(i==1){
+		content += "<a href='#' onclick='navClick(" + i + ")'><span class='done'><img src='images/thumblock.png'></img></span><p class='navigation'>" + i + "</p></a>";
+	}else{
+		content += "<a href='#' onclick='navClick(" + i + ")'><span><img src='images/thumblock.png'></img></span><p class='navigation'>" + i + "</p></a>";
+	}
+	pagesContentHolder.push([]);
+	soundContentHolder.push([]);
+	currentChapterPage.push(1);// all chapters into page 1 default
+	navigationCurrentPage.push(pagesPerChapter[i-1]+1);
+	for (var e = 1; e <= pagesPerChapter[i-1]; e++) { 
+		if(firstPage){
+			e--;
+			firstPage = false;
+			pagesContentHolder[i-1].push("blank");
+			soundContentHolder[i-1].push("blank");
+		}else{
+			totalPages++
+			pagesContentHolder[i-1].push("page"+totalPages);
+			soundContentHolder[i-1].push("audio"+totalPages);
+		}
+		
+	}
+	var firstPage = true;
+}
+
+loadScript("js/navigation.js", jsLoaded);
