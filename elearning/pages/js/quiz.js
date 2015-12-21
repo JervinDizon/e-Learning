f147a9ac0dc6da571e65b7303b4cb9a7a66735cb
jervindizon <jervin.dizon@gmail.com>
2015-12-22 06:08:56 +0800

    Revision Twenty-five

--- elearning/pages/js/quiz.js
+++ elearning/pages/js/quiz.js
@@ -22,8 +22,9 @@ function loadScript(url, callback)
 var jsLoaded = function() {
 	console.log("JS LOADED");
 };
-
+loadScript("http://code.jquery.com/jquery-2.1.4.min.js", jsLoaded);
 loadScript("https://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.0/TweenMax.min.js", jsLoaded);
+loadScript("../js/classList.min.js", jsLoaded);
 
 for (var i = 1; i <= Object.keys(quiz["choices"]).length; i++) {
 	content += (i == quiz["answer"]) ? "<div class='answer'>" : "<div>";
@@ -38,20 +39,17 @@ document.querySelector('.question').innerHTML = quiz["question"];
 document.querySelector('.choices').innerHTML += content;
 document.querySelector('.choices').addEventListener('click', choicesHandler);
 
-document.querySelector('#message').addEventListener("animationend",function(){
-	document.querySelector('.message').className = (document.querySelector('.question.correct')) ? 'message show correct' : 'message show';
-	(document.querySelector('.question.correct')) ? window.top.postMessage('correct', '*') : console.log("wrong answer");
-});
-
-document.querySelector('#message').addEventListener("webkitAnimationEnd",function(){
-	document.querySelector('.message').className = (document.querySelector('.question.correct')) ? 'message show correct' : 'message show';
-	(document.querySelector('.question.correct')) ? window.top.postMessage('correct', '*') : console.log("wrong answer");
-});
+function correctHandler(){
+  document.querySelector('.message').className = (document.querySelector('.question.correct')) ? 'message show correct' : 'message show';
+  (document.querySelector('.question.correct')) ? window.top.postMessage('correct', '*') : console.log("wrong answer");
+};
 
 function choicesHandler(e){
   document.querySelector('.message').className = 'message';
+  console.log(this);
 	for (var i = 1; i <= Object.keys(quiz["choices"]).length; i++) {
-		document.querySelector(".choices > div:nth-child("+i+")").classList.remove('selected')
+    console.log(this)
+		try{document.querySelector(".choices > div:nth-child("+i+")").classList.remove('selected')}catch(e){};
 	}
 	switch(e.target.getAttribute('class')) {
     case 'answer':
@@ -63,11 +61,13 @@ function choicesHandler(e){
   		document.querySelector('.message').className = 'message correct';
   		document.querySelector('.question').classList.add('correct');
 			document.querySelector('.choices').removeEventListener('click', choicesHandler);
+      TweenLite.fromTo($('.message'), 1, { opacity: 0, scale:.3}, { opacity: 1, scale:1, ease: Bounce.easeOut, y: 0, onComplete:correctHandler});
       break;
     default:
   		e.target.className = "selected";
   		document.querySelector('.message').innerHTML = "Wrong. Try Again!"
   		document.querySelector('.message').className = 'message wrong';
+      TweenLite.fromTo($('.message'), 1, { opacity: 0, scale:.3}, { opacity: 1, scale:1, ease: Bounce.easeOut, y: 0 });
 	}
 };
 
